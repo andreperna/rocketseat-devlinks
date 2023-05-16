6c979618bd45bfe5aa1fa2f61fe57f768be330b3
andreperna <andreperna@gmail.com>
2023-05-16 10:21:09 -0300

    delete 1 file and update 2 files

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function toggleMode(){
-  console.log("teste");
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+
+  // referencia img
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar-dark.png")
+  }
 }
